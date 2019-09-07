993778c6ab68794896cecf738c2e992c3b213ee4
Stephen Tanksley <stephen.tanksley@gmail.com>
2019-09-07 13:13:41 -0500

    Got the primary link functionality working. About to work on adding movies to the saved movie list.

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,8 @@
 import React, { useState } from 'react';
-
 import SavedList from './Movies/SavedList';
+import MovieList from './Movies/MovieList'
+import Movie from './Movies/Movie'
+import { Route } from 'react-router-dom'
 
 const App = () => {
   const [savedList, setSavedList] = useState( [] );
@@ -12,7 +14,15 @@ const App = () => {
   return (
     <div>
       <SavedList list={savedList} />
-      <div>Replace this Div with your Routes</div>
+      <Route exact path="/" component={MovieList} />
+      {/*When the browser's route ends with just "/", 
+        it will show the full list of films.*/}
+
+      <Route path="/movies/:id" component={Movie} />
+      {/*When the browser's route ends with the /:id of a film,
+        it substitute the value of that route and render the component
+        based on that. This is the same concept as template literals.*/}
+      
     </div>
   );
 };
--- client/src/Movies/Movie.js
+++ client/src/Movies/Movie.js
@@ -2,10 +2,10 @@ import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 
 const Movie = (props) => {
-  const [movie, setMovie] = useState({});
- 
+  const [movie, setMovie] = useState();
+  const id = props.match.params.id;
+  
   useEffect(() => {
-    const id = 1;
     // change ^^^ that line and grab the id from the URL
     // You will NEED to add a dependency array to this effect hook
 
@@ -13,6 +13,8 @@ const Movie = (props) => {
         .get(`http://localhost:5000/api/movies/${id}`)
         .then(response => {
           setMovie(response.data);
+          console.log(response.data)
+          console.log(movie);
         })
         .catch(error => {
           console.error(error);
@@ -20,6 +22,7 @@ const Movie = (props) => {
 
   },[]);
   
+
   // Uncomment this only when you have moved on to the stretch goals
   // const saveMovie = () => {
   //   const addToSavedList = props.addToSavedList;
